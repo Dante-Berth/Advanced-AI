d69b567733431767684cdc5aebe09e20ccbe54b5
Dante-Berth <bertinalexAI@protonmail.com>
2023-04-30 20:52:01 +0200

    adding new elements to be tested

--- Transformers/CustomTransformers.py
+++ Transformers/CustomTransformers.py
@@ -2,38 +2,89 @@ import tensorflow as tf
 import tensorflow_models as tfm
 
 
-print(type(tfm.nlp.layers.TalkingHeadsAttention()))
-
-class TransformersDecoder(tfm.nlp.layers.Transformer):
-    def __int__(self, num_attention_heads: int, inner_dim: int, inner_activation: tf.keras.layers.Layer, output_range: int, activation_layer: tf.keras.layers.Layer, *args, **kwargs):
-        """
-
-        :param num_attention_heads: int number of attention heads (different relations).
-        :param inner_dim: int the output dimension of the first Dense layer in a two-layer feedforward network.
-        :param inner_activation: int the activation for the first Dense layer in a two-layer feedforward network.
-        :param output_range: int the sequence output range, [0, output_range) for slicing the target sequence. None means the target sequence is not sliced.
-        :param activation_layer: tf.keras.layers.Layer  activation layer : TalkingHeadsAttention,GatedFeedforward,KernelAttention,MultiChannelAttention,TalkingHeadsAttention
-        :return: tf.keras.layers.Layer
-        """
-        self._num_attention_heads = num_attention_heads
-        self._inner_dim = inner_dim
-        self._inner_activation = inner_activation
-        self._output_range = output_range
-        self._activation_layer = activation_layer
-        super(TransformersDecoder, self).__init__(num_attention_heads, inner_dim, inner_activation, output_range, activation_layer, *args, **kwargs)
+class CustomTransformerEncoderBlock(tfm.nlp.layers.TransformerEncoderBlock):
+    def __init__(self, attention_layer, feedforward_layer, *args, **kwargs):
+        self.attention_layer = attention_layer
+        self.feedforward_layer = feedforward_layer
+        super(CustomTransformerEncoderBlock, self).__init__(*args, **kwargs)
 
     def build(self, input_shape):
-        # Replace the MultiHeadAttention layer with TalkingHeadsAttention
-        self.self.attention = self.activation_layer
+        super(CustomTransformerEncoderBlock, self).build(input_shape)
+        self._attention_layer = self.attention_layer
+        self._intermediate_dense = self.feedforward_layer
+
+    def call(self, inputs):
+        return super().call(inputs)
 
 
-tensor_4 = tf.random.uniform((32,24,9,3))
+
+custom_attention = tfm.nlp.layers.TalkingHeadsAttention(num_heads=8, key_dim=64)
+custom_ffn = tfm.nlp.layers.GatedFeedforward(inner_dim=1024, dropout=0.1, inner_activation="gelu")
+
+# Instantiate the custom TransformerEncoderBlock with the custom layers
+custom_transformer_block = CustomTransformerEncoderBlock(
+    attention_layer=custom_attention,
+    feedforward_layer=custom_ffn,
+    inner_activation="gelu",
+    num_attention_heads = 27,
+    key_dim = 64,
+    inner_dim=25
+)
 tensor_3 = tf.random.uniform((32,24,9))
+print(custom_transformer_block([tensor_3,None]))
 
-attention_mask = None
-encoder_block = tfm.nlp.layers.TransformerEncoderBlock(
-        num_attention_heads=2, inner_dim=10, inner_activation='relu')
-outputs = encoder_block([tensor_3, attention_mask])
-print(outputs)
+class LearnedPositionalEncoding(tf.keras.layers.Layer):
+    def __init__(self, max_position, d_model):
+        super(LearnedPositionalEncoding, self).__init__()
+        self.positional_encoding = self.add_weight("positional_encoding", shape=(max_position, d_model), initializer="zeros")
 
-TransformersDecoder(num_attention_heads=2, inner_dim=10, inner_activation='relu',)
+    def call(self, x):
+        seq_length = tf.shape(x)[1]
+        return x + self.positional_encoding[:seq_length, :]
+
+
+import tensorflow as tf
+
+class GPT(tf.keras.Model):
+    def __init__(self, vocab_size, d_model, nhead, num_layers):
+        super(GPT, self).__init__()
+        self.embedding = tf.keras.layers.Embedding(vocab_size, d_model)
+        self.positional_encoding = tf.keras.layers.Add()
+
+        self.decoder_layers = [
+            tf.keras.Sequential([
+                tf.keras.layers.LayerNormalization(),
+                tf.keras.layers.MultiHeadAttention(nhead, key_dim=d_model // nhead),
+                tf.keras.layers.Dropout(0.1),
+                tf.keras.layers.LayerNormalization(),
+                tf.keras.layers.Dense(d_model * 4, activation="gelu"),
+                tf.keras.layers.Dense(d_model),
+                tf.keras.layers.Dropout(0.1)
+            ])
+            for _ in range(num_layers)
+        ]
+
+        self.output_layer = tf.keras.layers.Dense(vocab_size)
+
+    def call(self, x, training=None):
+        seq_length = tf.shape(x)[1]
+        position = tf.range(0, seq_length, dtype=tf.float32)[:, tf.newaxis]
+        x = self.embedding(x)
+        x = self.positional_encoding(x + position)
+
+        look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((seq_length, seq_length)), -1, 0)
+        look_ahead_mask = look_ahead_mask[tf.newaxis, tf.newaxis, :, :] * -1e9
+
+        for decoder_layer in self.decoder_layers:
+            x = decoder_layer(x, attention_mask=look_ahead_mask, training=training)
+
+        logits = self.output_layer(x)
+        return logits
+
+# Instantiate the simple GPT model
+vocab_size = 10000
+d_model = 512
+nhead = 8
+num_layers = 6
+
+simple_gpt = GPT(vocab_size, d_model, nhead, num_layers)
