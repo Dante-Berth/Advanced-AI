37f4daacd19185b0c560ac948ab06b9b20861738
Dante-Berth <bertinalexAI@protonmail.com>
2023-05-11 22:10:14 +0200

    adding RNN layer almost finished

--- RNN/CustomRNN.py
+++ RNN/CustomRNN.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+
 def transform_tensor(tensor: tf.Tensor):
     """
     transform a tensor of length shape >=3 into a tensor of shape equals to 3
@@ -14,33 +15,76 @@ def transform_tensor(tensor: tf.Tensor):
 
     return transformed_tensor
 
+
 class RNN_Layer(tf.keras.layers.Layer):
     """
     RNN_layer generalizes the concept of RNN,  can handle input of different size either 3 or either 4
     """
 
-    def __init__(self, rnn_layer,units: int, return_sequences: bool, return_state: bool, dropout: float, recurrent_dropout: float, reduction:int):
+    def __init__(self, rnn_layer, units: int, dropout: float,
+                 recurrent_dropout: float, reduction_factor_input: int, reduction_factor_output: int,
+                 bidirectionnal_layer: bool):
+        """
+        Initialize the RNN_Layer.
+
+        :param rnn_layer: The specific RNN layer to use (e.g., tf.keras.layers.SimpleRNN, tf.keras.layers.LSTM).
+        :param units: The number of units or neurons in the RNN layer.
+        :param dropout: The dropout rate to apply to the RNN layer.
+        :param recurrent_dropout: The recurrent dropout rate to apply to the RNN layer.
+        :param reduction_factor_input: The reduction factor to apply to the input dimension.
+        :param reduction_factor_output: The reduction factor to apply to the output dimension.
+        :param bidirectionnal_layer:  A boolean indicating whether to use a bidirectional RNN layer or not.
+        """
         super(RNN_Layer, self).__init__()
         self.rnn_layer = getattr(tf.keras.layers, rnn_layer)
         self.units = units
-        self.return_sequences = return_sequences
-        self.return_state = return_state
         self.dropout = dropout
         self.recurrent_dropout = recurrent_dropout
-        self.reduction = reduction
+        self.reduction_factor_input = reduction_factor_input
+        self.reduction_factor_output = reduction_factor_output
+        self.bidirectionnal = bidirectionnal_layer
+        self.reduction_layer_input = None
+        self.reduction_layer_output = None
 
     @staticmethod
     def RNN_layer_hyperparemeters():
         return {
-            "hyperparameter_rnn_layer": ["SimpleRNN","LSTM","GRU"],
-            "hyperparameter_units": [1, 64],
-            "hyperparameter_return_sequences": [True, False],
-            "hyperparameter_return_state": [True, False],
-            "hyperparameter_dropout": [0,0.5,0.1],
-            "hyperparameter_recurrent_dropout": [0, 0.5, 0.1]
-            "hyperparameter_reduction":[8,256]
+            "hyperparameter_rnn_layer": ["SimpleRNN", "LSTM", "GRU"],
+            "hyperparameter_units": [2, 256],
+            "hyperparameter_dropout": [0, 0.5, 0.1],
+            "hyperparameter_recurrent_dropout": [0, 0.5, 0.1],
+            "hyperparameter_reduction_factor_input": [1, 32],
+            "hyperparameter_reduction_factor_output": [1, 16],
+            "hyperparameter_bidirectionnal_layer": [True, False]
         }
-if __name__=="__main__":
-    tensor_3 = tf.ones((12,24,36))
-    tensor_4 = tf.ones((12,24,36,48))
-    print(tf.keras.layers.Conv1D(22,1)(transform_tensor(tensor_4)))
+
+    def build(self, input_shape):
+        self.reduction_layer_input = tf.keras.layers.Conv1D(filters=input_shape[-1] // self.reduction_factor_input,
+                                                            kernel_size=1)
+        if self.bidirectionnal:
+            self.rnn_layer = tf.keras.layers.Bidirectional(self.rnn_layer(units=self.units,
+                                                                          return_sequences=True,
+                                                                          dropout=self.dropout,
+                                                                          recurrent_dropout=self.recurrent_dropout
+                                                                          ))
+        else:
+            self.rnn_layer = self.rnn_layer(units=self.units,
+                                            return_sequences=True,
+                                            dropout=self.dropout,
+                                            recurrent_dropout=self.recurrent_dropout
+                                            )
+        self.reduction_layer_output = tf.keras.layers.Conv1D(filters=self.units // self.reduction_factor_output,
+                                                             kernel_size=1)
+
+    def call(self, inputs, **kwargs):
+        x = self.reduction_layer_input(inputs)
+        x = self.rnn_layer(x)
+        return self.reduction_layer_output(x)
+
+
+if __name__ == "__main__":
+    tensor_3 = tf.ones((12, 24, 36))
+    tensor_4 = tf.ones((12, 24, 36, 48))
+    print(tf.keras.layers.LSTM(22, return_sequences=True)(tensor_3))
+    print(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(12, return_sequences=True))(tensor_3))
+    print(RNN_Layer("LSTM",12,0.3,0.3,2,2,False)(tensor_3))
