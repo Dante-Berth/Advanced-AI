774c29258cf61a06417f05f2c821c9daa2ecd117
Dante-Berth <bertinalexAI@protonmail.com>
2023-05-18 20:09:58 +0200

    minor changes

--- HyperOptimization/optimizer_optuna.py
+++ HyperOptimization/optimizer_optuna.py
@@ -1,29 +1,23 @@
 import optuna
 import importlib.util
 import tensorflow as tf
-
 from CNN.CustomCNN import CNN_Layer
 from MLP.CustomPerceptron import Perceptron_Layer
 from RNN.CustomRNN import RNN_Layer
-from Transformers.CustomTransformers import CustomTransformerEncoderBlock
-
+from Transformers.CustomTransformers import TransformerEncoderBlock_layer
 
 import tensorflow as tf
 
-class TransformTensor(tf.keras.layers.Layer):
-    def __init__(self):
-        super(TransformTensor, self).__init__()
+# Load the MNIST dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-    def call(self, tensor):
-        shape = tf.shape(tensor)
-        batch_size = shape[0]
-        sequence_length = shape[1]
-        embedding_size = tf.reduce_prod(shape[2:])
-        transformed_tensor = tf.reshape(tensor, (batch_size, sequence_length, embedding_size))
+# Normalize pixel values between 0 and 1
+x_train = x_train.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
 
-        return transformed_tensor
-
-
+# Add a channel dimension (for grayscale images)
+x_train = tf.expand_dims(x_train, -1)
+x_test = tf.expand_dims(x_test, -1)
 def OptunaListElements(name_layer,liste,key,trial):
     if type(liste[0])==int:
         if len(liste)==2:
@@ -39,44 +33,50 @@ def loop_initializer(layer,trial,i,j):
     liste_hyperparameters = [OptunaListElements(f'{name_layer}_deep_{i}_width_{j}',hyperparameters[key],key.split(".")[-1].replace(prefix,""),trial) for key in hyperparameters.keys()]
     return liste_hyperparameters
 
-# Load the MNIST dataset
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+class Reshape_Layer(tf.keras.layers.Layer):
+    def __init__(self):
+        super(Reshape_Layer, self).__init__()
+        self.reshape = None
 
-# Normalize pixel values between 0 and 1
-x_train = x_train.astype("float32") / 255.0
-x_test = x_test.astype("float32") / 255.0
+    def build(self, input_shape):
+        if len(input_shape)>3:
+            self.reshape = tf.keras.layers.Reshape((input_shape[1], tf.reduce_prod(input_shape[2:]).numpy()[()]))
+    def call(self, inputs, *args, **kwargs):
+        if self.reshape:
+            return self.reshape(inputs)
+        else:
+            return inputs
+def objective(trial,x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test):
+    input_layer = tf.keras.layers.Input(shape=(28, 28, 1))
 
-# Add a channel dimension (for grayscale images)
-x_train = tf.expand_dims(x_train, -1)
-x_test = tf.expand_dims(x_test, -1)
-def objective(trial,x_train=x_train,x_test=x_test,y_train=y_train,y_test=y_test):
-    model = tf.keras.Sequential()
+    x = CNN_Layer(*loop_initializer(CNN_Layer, trial, 1, 1))(input_layer)
+
+    x = Reshape_Layer()(x)
 
 
-    # Add the convolutional layers and a perceptron
-    model.add(CNN_Layer(*loop_initializer(CNN_Layer,trial,1,1)))
-    model.add(TransformTensor())
-    model.add(RNN_Layer(*loop_initializer(RNN_Layer,trial,1,2)))
-    model.add(Perceptron_Layer(*loop_initializer(Perceptron_Layer,trial,1,3)))
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(10, activation="softmax"))
+    y = RNN_Layer(*loop_initializer(RNN_Layer, trial, 1, 2))(x)
 
+    y = Perceptron_Layer(*loop_initializer(Perceptron_Layer, trial, 1, 3))(y)
+
+    # Flatten the output
+    y = tf.keras.layers.Flatten()(y)
+
+    # Define the output layer
+    output = tf.keras.layers.Dense(10, activation="softmax")(y)
+
+    model = tf.keras.models.Model(inputs=input_layer, outputs=output)
+    print(model.summary())
+    exit()
     model.compile(
         optimizer="adam",
         loss=tf.keras.losses.SparseCategoricalCrossentropy(),
         metrics=["accuracy"]
     )
-
     model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test), verbose=1)
 
     _, accuracy = model.evaluate(x_test, y_test)
 
     return -accuracy
-
 study = optuna.create_study(direction="maximize")
 study.optimize(objective, n_trials=5)
 
-"""
-I should create a function which takes in input a layer, then open the dictionnary of hyperparameters and then instances the object and add the hyperparameters in the trial\
-I am a bit exhausted to continue but it can be easily done. Use the tips CNN_layer(*list) * unpack elements like lists, tuples, or strings
-"""
