9c15059dc13d47b4d13bdf05360339cd9674a9f2
Dante-Berth <bertinalexAI@protonmail.com>
2023-05-20 19:50:42 +0200

    almost finished the job

--- Testing.py
+++ Testing.py
@@ -1,34 +1,60 @@
-import random
-b,c,d = 4,12,9
-print(b,c,d)
-tensor_3_1 = tf.ones((12, b, c))
-tensor_4_1 = tf.ones((12, c, b, d))
-tensor_3_2 = tf.ones((12, d, c))
-tensor_4_2 = tf.ones((12, b, b, d))
+import tensorflow as tf
+class tensor_list_tensor(tf.keras.layers.Layer):
+    def __init(self):
+        super(tensor_list_tensor).__init()
+        self.bool_padding = True
+    def call(self, x):
+        if isinstance(x[1], tf.Tensor):
+            x[0], x[1] = self.creator_pad(x[0],x[1])
+            y = x[0] + x[1]
+    def creator_pad(self,tensor_1,tensor_2):
+        if self.instance_padding:
+            self.padding(tensor_1,tensor_2)
+        return tf.pad(tensor_1, self.padding_1), tf.pad(tensor_2, self.padding_2)
+
+    def padding(self,tensor_1,tensor_2):
+        shape_1 = tensor_1.shape
+        shape_2 = tensor_2.shape
+        if shape_1 > shape_2:
+            tensor_2 = tf.expand_dims(tensor_2,axis=-1)
+            shape_2 = tensor_2.shape
+
+        elif shape_1<shape_2:
+            tensor_1 = tf.expand_dims(tensor_2, axis=-1)
+            shape_1 = tensor_1.shape
+
+        padding_1 = []
+        padding_2 = []
+        for i, j in zip(shape_1, shape_2):
+            padding_1.append([0, max(j - i)])
+            padding_2.append([0, max(i - j)])
+
+        self.padding_1 = padding_1
+        self.padding_2 = padding_2
+        self.instance_padding = False
+
+
+
+
 
 # Input vector with size (4, 5, 6)
-vector_1 = tf.ones(shape=(b, d, c))
-vector_2 = tf.ones(shape=(c,b,d))
+vector_1 = tf.ones(shape=(12, 5, 6))
+vector_2 = tf.ones(shape=(10,8,14))
 shape_1 = vector_1.shape
 shape_2 = vector_2.shape
 
 pad_first = shape_1[0] - shape_2[0]
 pad_second = shape_1[1] - shape_2[1]
 pad_three = shape_1[2] - shape_2[2]
-
-
 # Pad the input vector
-padded_vector = tf.pad(vector_1, [[0, int(tf.keras.activations.relu(0,pad_first).numpy())], [0, int(tf.keras.activations.relu(0,pad_second).numpy())], [0, int(tf.keras.activations.relu(0,pad_three).numpy())]])
-print(shape_1)
-padded_vector_2 = tf.pad(vector_2, [[0, int(tf.keras.activations.relu(-pad_first,0).numpy())], [0, int(tf.keras.activations.relu(0,-pad_second).numpy())], [0, int(tf.keras.activations.relu(0,-pad_three).numpy())]])
-print(shape_2)
-print("Padded Vector:")
-print(padded_vector)
-print("Padded Vector Shape:", padded_vector.shape)
-print("Padded Vector:")
-print(padded_vector_2)
-print("Padded Vector Shape:", padded_vector_2.shape)
+padded_vector = tf.pad(vector_1, [[0, max(0,-pad_first)], [0, max(0,-pad_second)], [0, max(0,-pad_three)]])
+padded_vector_2 = tf.pad(vector_2, [[0, max(0,pad_first)], [0, max(0,pad_second)], [0, max(0,pad_three)]])
 
+print("Padded Vector Shape:", padded_vector.shape )
+print("Padded Vector Shape:", padded_vector_2.shape)
+tf.keras.layers.Concatenate(axis=-1)([padded_vector,padded_vector_2])
+print("success")
+exit()
 
 import tensorflow as tf
 
@@ -41,3 +67,7 @@ padded_matrix = tf.pad(input_matrix, [[1, 1], [1, 1]])
 
 print("Padded Matrix:")
 print(padded_matrix)
+
+exit()
+print()
+
