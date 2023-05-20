e153572fbe3ddac1c0eca6a086390f7706881c0f
Dante-Berth <bertinalexAI@protonmail.com>
2023-05-20 21:15:19 +0200

    Layer which transform a two list tensors into a list of one tensor thanks to 0 padding

---
+++ Fromtwotensorsintoonetensor.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+class TensorListTensor(tf.keras.layers.Layer):
+    def __init__(self):
+        super(TensorListTensor, self).__init__()
+        self.instance_padding = True
+
+    def call(self, x):
+        if isinstance(x[1], tf.Tensor):
+            x[0], x[1] = self.creator_pad(x[0],x[1])
+            y = x[0] + x[1]
+        else:
+            y = x[0]
+        return y
+    def creator_pad(self,tensor_1,tensor_2):
+        if self.instance_padding:
+            self.padding(tensor_1,tensor_2)
+
+        shape_1 = tensor_1.shape
+        shape_2 = tensor_2.shape
+        if len(shape_1) > len(shape_2):
+            tensor_2 = tf.expand_dims(tensor_2,axis=-1)
+        elif len(shape_1)<len(shape_2):
+            tensor_1 = tf.expand_dims(tensor_1, axis=-1)
+
+
+        return tf.pad(tensor_1, self.padding_1.numpy().tolist()), tf.pad(tensor_2, self.padding_2.numpy().tolist())
+
+    def padding(self,tensor_1,tensor_2):
+        shape_1 = tensor_1.shape
+        shape_2 = tensor_2.shape
+        if len(shape_1) > len(shape_2):
+            tensor_2 = tf.expand_dims(tensor_2,axis=-1)
+            shape_2 = tensor_2.shape
+
+        elif len(shape_1)<len(shape_2):
+            tensor_1 = tf.expand_dims(tensor_1, axis=-1)
+            shape_1 = tensor_1.shape
+
+        padding_1 = []
+        padding_2 = []
+        print(shape_1,shape_2)
+        for i, j in zip(shape_1, shape_2):
+            print(i,j)
+            padding_1.append([0, max(0,j - i)])
+            padding_2.append([0, max(0,i - j)])
+
+        self.padding_1 = tf.constant(padding_1)
+        self.padding_2 = tf.constant(padding_2)
+        self.instance_padding = False
+
+
+
+
+
+# Input vector with size (4, 5, 6)
+vector_1 = tf.ones(shape=(12, 5, 6))
+vector_2 = tf.ones(shape=(10,8,14))
+shape_1 = vector_1.shape
+shape_2 = vector_2.shape
+
+pad_first = shape_1[0] - shape_2[0]
+pad_second = shape_1[1] - shape_2[1]
+pad_three = shape_1[2] - shape_2[2]
+# Pad the input vector
+padded_vector = tf.pad(vector_1, [[0, max(0,-pad_first)], [0, max(0,-pad_second)], [0, max(0,-pad_three)]])
+padded_vector_2 = tf.pad(vector_2, [[0, max(0,pad_first)], [0, max(0,pad_second)], [0, max(0,pad_three)]])
+
+print([[0, max(0,-pad_first)], [0, max(0,-pad_second)], [0, max(0,-pad_three)]],"padded_vector")
+vector_1 = tf.ones(shape=(12, 5, 6))
+vector_2 = tf.ones(shape=(10,8,14))
+print(TensorListTensor()([vector_1, None]))
+exit()
