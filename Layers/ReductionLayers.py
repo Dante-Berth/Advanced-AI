d1b7a342087bbabdd2c94032aea04296d079b051
Dante-Berth <bertinalexAI@protonmail.com>
2023-06-08 22:45:22 +0200

    adding a dense layer for reducing the last dimension in ReductionLayerPooling

--- Layers/ReductionLayers.py
+++ Layers/ReductionLayers.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from Fromtwotensorsintoonetensor import R_ListTensor
 from CNN.CustomCNN import MetaPoolingLayer
 
+
 class ReductionLayerSVD(tf.keras.layers.Layer):
     def __init__(self, r):
         super(ReductionLayerSVD, self).__init__()
@@ -20,30 +21,31 @@ class ReductionLayerSVD(tf.keras.layers.Layer):
 
 
 class ReductionLayerPooling(tf.keras.layers.Layer):
-    def __init__(self, pool_size: int, strides: int, pooling_layer_name: str):
+    def __init__(self, ratio_pool_size: int, ratio_strides: int, ratio_dense: int, pooling_layer_name: str):
         super(ReductionLayerPooling, self).__init__()
-        self.pool_size = pool_size
-        self.strides = strides
+        self.ratio_pool_size = ratio_pool_size
+        self.ratio_strides = ratio_strides
+        self.ratio_dense = ratio_dense
         self.pooling_layer_name = pooling_layer_name
         self.pooling_layer = None
+
     @staticmethod
     def get_layer_hyperparameters():
         return {
-            "hyperparameter_pool_size": [1, 10, 1],
-            "hyperparameter_strides": [1, 10, 1],
+            "hyperparameter_ratio_pool_size": [1, 3, 1],
+            "hyperparameter_ratio_strides": [1, 3, 1],
+            "hyperparameter_ratio_dense": [1, 7, 1],
             "hyperparameter_pooling_layer_name": ["MetaPoolingLayer", "AveragePooling2D", "MaxPooling2D",
                                                   "AveragePooling1D", "MaxPooling1D"]
         }
+
     def build(self, input_shape):
-        if isinstance(input_shape,list):
+        if isinstance(input_shape, list):
             self.R_ListTensor = R_ListTensor()
             input_shape = self.R_ListTensor.get_output_shape(input_shape)
 
-        input_dim_1 = input_shape[1]
-        input_dim_2 = input_shape[2]
-
-        self.pool_size = input_shape[1]*self.ratio_pool_size*0.1//1
-        self.strides = input_shape[2]*self.ratio_strides*0.1//1
+        self.pool_size = input_shape[1] * self.ratio_pool_size // 10
+        self.strides = input_shape[2] * self.ratio_strides // 10
 
         if self.pooling_layer_name == "MetaPoolingLayer":
             self.pooling_layer = MetaPoolingLayer(self.pool_size, self.strides)
@@ -60,27 +62,28 @@ class ReductionLayerPooling(tf.keras.layers.Layer):
                     self.pooling_layer_name = "MaxPooling2D"
 
             self.pooling_layer = getattr(tf.keras.layers, self.pooling_layer_name)(self.pool_size, self.strides,
-                                                                                           padding="valid",
-                                                                                           data_format="channels_last")
+                                                                                   padding="valid",
+                                                                                   data_format="channels_last")
+        output_shape_pooling_layer = self.pooling_layer.compute_output_shape(input_shape)
+
+        self.dense_layer = tf.keras.layers.Dense(units=input_shape[-1] * self.ratio_dense // 10)
+        self.dense_layer.build(output_shape_pooling_layer)
 
     def call(self, input):
-        if isinstance(input,list):
+        if isinstance(input, list):
             input = self.R_ListTensor.call(input)
-        return self.pooling_layer(input)
+        x = self.pooling_layer(input)
+        return self.dense_layer(x)
+
+
 if __name__ == "__main__":
     tensor_3 = tf.random.uniform((12, 24, 36))
     tensor_4 = tf.random.uniform((12, 24, 36, 48))
-    tensor_5 = tf.random.uniform((12, 6, 6, 1))
-    tensor_6 = tf.random.uniform((12, 2, 2))
     linalgmonolayer = ReductionLayerSVD(50)
 
     # Pass the input tensor through the layer
     output = linalgmonolayer(tensor_4)
 
-
-    #print(ReductionLayerPooling(3, 3, "AveragePooling1D")(tensor_4).shape)
-    #print(ReductionLayerPooling(3, 3, "MetaPoolingLayer")([tensor_3, tensor_3]).shape)
-    #print(ReductionLayerPooling(18, 20, "MetaPoolingLayer")([tensor_3, tensor_3]).shape)
-    print(tf.keras.layers.MaxPool2D(6,6)(tensor_4).shape)
-    print(tf.keras.layers.MaxPool2D(6, 6)(tensor_5).shape)
-    print(tf.keras.layers.MaxPool1D(3, 5)(tensor_6).shape)
+    print(ReductionLayerPooling(2, 2, 4, "AveragePooling1D")(tensor_4).shape)
+    print(ReductionLayerPooling(10, 10, 3, "MetaPoolingLayer")([tensor_3, tensor_3]).shape)
+    print(ReductionLayerPooling(5, 6, 2, "MetaPoolingLayer")([tensor_3, tensor_4]).shape)
