b8355da70a15785afd223b20ff55401fa239c791
Dante-Berth <bertinalexAI@protonmail.com>
2023-07-16 20:39:38 +0200

    Activation and Layers can be saved

--- Activation/CustomActivationLayers.py
+++ Activation/CustomActivationLayers.py
@@ -45,49 +45,49 @@ class Signlog(tf.keras.layers.Layer):
 
     def get_config(self):
         config = super(Signlog, self).get_config()
-
         return config
 
     def call(self, inputs):
         return tf.math.sign(inputs) * tf.math.log(tf.keras.activations.relu(self.weight)*tf.math.abs(inputs) + 1)
 
 @tf.keras.utils.register_keras_serializable()
-class Signlog_Expcos(tf.keras.layers.Layer):
+class MetaActivationLayer(tf.keras.layers.Layer):
 
-    def __init__(self,signlog,expcos,**kwargs):
-        super(Signlog_Expcos, self).__init__(**kwargs)
-        self.signlog = signlog
-        self.expcos = expcos
+    def __init__(self,**kwargs):
+        super(MetaActivationLayer, self).__init__(**kwargs)
+        self.signlog = Signlog()
+        self.expcos = Expcos()
+        self.weight_1 = self.add_weight(
+            name='weights_1',
+            shape=(1,),
+            initializer=tf.keras.initializers.RandomUniform(minval=-10, maxval=10),
+            trainable=True
+        )
+        self.weight_2 = self.add_weight(
+            name='weights_2',
+            shape=(1,),
+            initializer=tf.keras.initializers.RandomUniform(minval=-10, maxval=10),
+            trainable=True
+        )
     def get_config(self):
-        config = super(Signlog_Expcos, self).get_config()
-        config.update({
-            'signlog': tf.keras.layers.serialize(self.signlog),
-            'expcos': tf.keras.layers.serialize(self.expcos)
-        })
+        config = super(MetaActivationLayer, self).get_config()
+        config.update(self.expcos.get_config())
+        config.update(self.signlog.get_config())
         return config
 
-    @classmethod
-    def from_config(cls, config):
-        signlog = tf.keras.layers.deserialize(config.pop('signlog'))
-        expcos = tf.keras.layers.deserialize(config.pop('expcos'))
-        return cls(signlog=signlog, expcos=expcos, **config)
     def call(self, inputs):
-        print("entergint in  the layer")
-        return self.signlog(inputs) + self.expcos(inputs)
+        return self.weight_1*self.signlog(inputs) + self.weight_2*self.expcos(inputs)
 
 
 @tf.keras.utils.register_keras_serializable()
-class MetaActivationLayer(tf.keras.layers.Layer):
+class MetaActivationLayer_2(tf.keras.layers.Layer):
     """
     Idea taken from AutoML Springer P.66
     """
-    def __init__(self, **kwargs):
+    def __init__(self,**kwargs):
         super(MetaActivationLayer, self).__init__(**kwargs)
-        self.activation_gelu = tf.keras.activations.gelu
-        self.activation_softmax = tf.keras.activations.softmax
-        self.activation_softsign = tf.keras.activations.softsign
-        self.activation_Signlog = Signlog(**kwargs)
-        self.activation_Expcos = Expcos(**kwargs)
+        self.activation_Signlog = Signlog()
+        self.activation_Expcos = Expcos()
 
         self.num_weights = 5
         self.weights_list = self.add_weight(
@@ -99,15 +99,28 @@ class MetaActivationLayer(tf.keras.layers.Layer):
         self.activation_list = None
     def get_weights(self):
         return self.weights_list
-
-    def get_config(self,**kwargs):
-        config = super(MetaActivationLayer, self).get_config(**kwargs)
+    def get_config(self):
+        config = super(MetaActivationLayer, self).get_config()
+        config.update({
+            'gelu': tf.keras.layers.serialize(self.activation_gelu),
+            'softmax': tf.keras.layers.serialize(self.activation_softmax),
+            'softsign': tf.keras.layers.serialize(self.activation_softsign),
+            'signlog': tf.keras.layers.serialize(self.activation_Signlog),
+            'expcos': tf.keras.layers.serialize(self.activation_Expcos)
+        })
         return config
 
+    @classmethod
+    def from_config(cls, config):
+        gelu = tf.keras.layers.deserialize(config.pop('gelu'))
+        softmax = tf.keras.layers.deserialize(config.pop('softmax'))
+        softsign = tf.keras.layers.deserialize(config.pop('softsign'))
+        signlog = tf.keras.layers.deserialize(config.pop('signlog'))
+        expcos = tf.keras.layers.deserialize(config.pop('expcos'))
+        return cls(gelu=gelu, softmax=softmax, softsign=softsign ,signlog=signlog, expcos=expcos,**config)
+
     def call(self, inputs, **kwargs):
-        print(self.activation_list,"activation_list")
         if self.activation_list is None:
-            print("there")
             self.activation_list = [
                 self.activation_gelu,
                 self.activation_softmax,
@@ -115,16 +128,14 @@ class MetaActivationLayer(tf.keras.layers.Layer):
                 self.activation_Signlog,
                 self.activation_Expcos
             ]
-            print(self.activation_list)
         weighted_activations = tf.zeros_like(inputs)
         for weight, activation in zip(tf.unstack(self.weights_list, axis=0), self.activation_list):
             weighted_activations += weight * activation(inputs)
         return weighted_activations
 if __name__=="__main__":
     tensor_123 = tf.ones((4, 5, 6, 7))
-    print(MetaActivationLayer()(tensor_123))
     input_layer = tf.keras.layers.Input(shape=(4, 5, 6, 7))
-    output = Signlog_Expcos(signlog=Signlog(),expcos=Expcos())(input_layer)
+    output = MetaActivationLayer()(input_layer)
 
 
 
@@ -134,11 +145,15 @@ if __name__=="__main__":
         loss=tf.keras.losses.SparseCategoricalCrossentropy(),
         metrics=["accuracy"]
     )
+    print("Model before the loading")
     model.summary()
 
     PATH = 'testing_model_custom_activation_layer.h5'
     model.save(PATH)
     model = tf.keras.models.load_model(PATH)
+    print("Model loaded")
+    print(model.summary())
+
     """
     try it
         def get_config(self):
