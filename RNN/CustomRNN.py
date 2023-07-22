8592279018374b46427858450ce4b30202050313
Dante-Berth <bertinalexAI@protonmail.com>
2023-07-23 00:36:49 +0200

    solved RNN

--- RNN/CustomRNN.py
+++ RNN/CustomRNN.py
@@ -26,7 +26,7 @@ class RNN_Layer(tf.keras.layers.Layer):
     """
 
     def __init__(self, rnn_layer_str,units: int, dropout: float,
-                 recurrent_dropout: float, reduction_factor_input: int, reduction_factor_output: int,**kwargs):
+                 recurrent_dropout: float, reduction_factor_input: int, reduction_factor_output: int,*args,**kwargs):
         """
         Initialize the RNN_Layer.
         :param units: The number of units or neurons in the RNN layer.
@@ -36,7 +36,7 @@ class RNN_Layer(tf.keras.layers.Layer):
         :param reduction_factor_output: The reduction factor to apply to the output dimension.
         :param rnn_layer: The specific RNN layer to use (e.g., tf.keras.layers.SimpleRNN, tf.keras.layers.LSTM).
         """
-        super(RNN_Layer, self).__init__(**kwargs)
+        super(RNN_Layer, self).__init__(*args,**kwargs)
         self.rnn_layer_str = rnn_layer_str
         self.rnn_layer = getattr(tf.keras.layers, rnn_layer_str)
         self.units = units
@@ -44,6 +44,7 @@ class RNN_Layer(tf.keras.layers.Layer):
         self.recurrent_dropout = recurrent_dropout//10*0.1
         self.reduction_factor_input = reduction_factor_input
         self.reduction_factor_output = reduction_factor_output
+        self.reshape_layer = Reshape_Layer_3D()
 
     def get_config(self):
         config = super(RNN_Layer, self).get_config()
@@ -56,22 +57,29 @@ class RNN_Layer(tf.keras.layers.Layer):
             'reduction_factor_output': self.reduction_factor_output
         })
         return config
-    @staticmethod
-    def transform_tensor(tensor: tf.Tensor):
-        """
-        transform a tensor of length shape >=3 into a tensor of shape equals to 3
-        :param tensor: tf.Tensor is the tensor of length shape >3
-        :return: a of tensors of shape (batch_size, sequence_length, embedding_size)
-        """
-        shape = tf.shape(tensor)
-        batch_size = shape[0]
-        sequence_length = shape[1]
-        embedding_size = tf.reduce_prod(shape[2:])
-        transformed_tensor = tf.reshape(tensor, (batch_size, sequence_length, embedding_size))
 
-        return transformed_tensor
+    @staticmethod
+    def get_name():
+        return "rnn"
+
+    @staticmethod
+    def get_layer_hyperparemeters():
+        return {
+            "hyperparameter_rnn_layer": ["SimpleRNN", "LSTM", "GRU"],
+            "hyperparameter_units": [2, 256],
+            "hyperparameter_dropout": [0, 50, 10],
+            "hyperparameter_recurrent_dropout": [0, 50, 10],
+            "hyperparameter_reduction_factor_input": [1, 32],
+            "hyperparameter_reduction_factor_output": [1, 16]
+        }
 
     def build(self, input_shape):
+
+        if isinstance(input_shape,list):
+            self.R_ListTensor = RListTensor()
+            input_shape = self.R_ListTensor.get_output_shape(input_shape)
+        self.reshape_layer.build(input_shape)
+        input_shape = self.reshape_layer.compute_output_shape(input_shape)
 
         self.reduction_layer_input = tf.keras.layers.Conv1D(filters=tf.maximum(1,input_shape[-1] // self.reduction_factor_input),
                                                             kernel_size=1)
@@ -82,17 +90,21 @@ class RNN_Layer(tf.keras.layers.Layer):
                                             dropout=self.dropout,
                                             recurrent_dropout=self.recurrent_dropout
                                             )
+
         self.reduction_layer_output = tf.keras.layers.Conv1D(filters=tf.maximum(1,self.units // self.reduction_factor_output),
                                                              kernel_size=1)
 
     def call(self, inputs):
-        x = self.reduction_layer_input(inputs)
+        if isinstance(inputs,list):
+            inputs = self.R_ListTensor.call(inputs)
+        x = self.reshape_layer.call(inputs)
+        x = self.reduction_layer_input(x)
         x = self.rnn_layer(x)
         return self.reduction_layer_output(x)
 @tf.keras.utils.register_keras_serializable()
 class R_RNN_Layer(RNN_Layer):
-    def __init__(self,**kwargs):
-        super(R_RNN_Layer, self).__init__(**kwargs)
+    def __init__(self,*args,**kwargs):
+        super(R_RNN_Layer, self).__init__(*args,**kwargs)
         self.reshape_layer = Reshape_Layer_3D()
 
     @staticmethod
@@ -136,7 +148,7 @@ if __name__ == "__main__":
 
 
     # Create an instance of ReshapeAndRNNLayer
-    reshape_rnn_layer = R_RNN_Layer(rnn_layer_str="LSTM", units=12, dropout=0.3, recurrent_dropout=0.3,
+    reshape_rnn_layer = RNN_Layer(rnn_layer_str="LSTM", units=12, dropout=0.3, recurrent_dropout=0.3,
                                            reduction_factor_input=2, reduction_factor_output=2)
 
     # Pass the input tensor through the layer
@@ -148,7 +160,7 @@ if __name__ == "__main__":
     print(output.shape)
 
     # Create an instance of ReshapeAndRNNLayer
-    reshape_rnn_layer = R_RNN_Layer(rnn_layer_str="LSTM", units=12, dropout=0.3, recurrent_dropout=0.3,
+    reshape_rnn_layer = RNN_Layer(rnn_layer_str="LSTM", units=12, dropout=0.3, recurrent_dropout=0.3,
                                            reduction_factor_input=2, reduction_factor_output=2)
 
     # Pass the input tensor through the layer
@@ -156,8 +168,11 @@ if __name__ == "__main__":
 
     # Print the output shape
     print(output.shape)
-    rnn_layer_test = R_RNN_Layer(rnn_layer_str="LSTM", units=12, dropout=0.3, recurrent_dropout=0.3,
-                                           reduction_factor_input=2, reduction_factor_output=2)
+    dictionnary = {"rnn_layer_str":"LSTM", "units":12, "dropout":0.3, "recurrent_dropout":0.3,
+                                           "reduction_factor_input":2, "reduction_factor_output":2 }
+    liste = ["LSTM", 12, 0.3, 0.3,2, 2]
+
+    rnn_layer_test = RNN_Layer(*liste)
     vector_1 = tf.keras.layers.Input(shape=(5, 12, 2))
     vector_2 = tf.keras.layers.Input(shape=(5,12))
     vector_3 = [vector_1, vector_2]
